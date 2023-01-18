374d97303c778e932348c48e1e896dad70ac9ae2
Isaiah White-Booth <booth2116@outlook.com>
2023-01-17 21:40:33 -0500

    Initial Commit, Random Character Attribute Generator Code --

---
+++ Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LegendOfGasbar
+{
+    public class Character 
+    { 
+        public string c_name { get; set; }
+        public int c_speed { get; set; }
+        public int c_power { get; set; }
+        public int c_limit { get; set; }
+        public int c_health { get; set; }
+
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace LegendOfGasbar
 {
@@ -17,6 +19,37 @@ namespace LegendOfGasbar
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            // You need to bring in the prototype character object
+            Character myCharacter = new Character();
+
+            // Generate Random Values for Character
+            Random rnd = new Random();
+            myCharacter.c_limit = rnd.Next(0, 100000);
+            myCharacter.c_health = rnd.Next(0, 100);
+            myCharacter.c_power = rnd.Next(0, 999);
+            myCharacter.c_speed = rnd.Next(1, 10);
+
+            // Logic for the remaining items -> Random Name
+            string[] randomNames = { "Axel", "Thrasher", "Xcel", "DangerMan", "The Rock", "MegurManX" };
+            myCharacter.c_name = randomNames[rnd.Next(0, 5)];
+
+            // Show the player the character build
+            MessageBox.Show 
+                (
+                "Name : \t" + myCharacter.c_name + "\n" + "Power Level: \t " +
+                myCharacter.c_power.ToString() + "\n" + "Character Health: \t" 
+                + myCharacter.c_health.ToString() + "\n" + "Character Speed: \t" + myCharacter.c_speed.ToString() 
+                + "\n" + "Power Limit: \t" + myCharacter.c_limit.ToString(), "Character Build"
+                );
+      
+
+            // Capture the character in a holder (Save File???)
+            List<Character> list = new List<Character>();
+            list.Add(myCharacter);
+            
+            
+           
         }
     }
 }
